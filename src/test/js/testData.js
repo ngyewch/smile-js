100b1df83979dbb7fa1636ca948314da7c7257e7
Ng Yew Choong <yc@affle.com>
2015-07-11 23:43:01 +0800

    Updated test data.

--- src/test/js/testData.js
+++ src/test/js/testData.js
@@ -3,3 +3,5 @@ testData['basicArray.min.json'] = '[\"hello\",123,5.0]';
 testData['basicArray.smile'] = 'OikKAfhEaGVsbG8kA7YpAEAKAAAAAAAAAPk=';
 testData['basicObject.min.json'] = '{\"name\":\"Bob\",\"age\":25,\"score\":96.8}';
 testData['basicObject.smile'] = 'OikKAfqDbmFtZUJCb2KCYWdlJLKEc2NvcmUpAEAsDGYzGUxmM\/s=';
+testData['basicSimpleLiteralValues.min.json'] = '{\"emptyString\":\"\",\"null\":null,\"false\":false,\"true\":true,\"int32\":2147483647,\"int64_1\":2147483648,\"int64_2\":9007199254740991,\"int64_3\":9007199254740992,\"int64_4\":9223372036854775807,\"bigint\":9223372036854775808,\"float32\":1234.567,\"float64\":1.234567812345679E7,\"bigdecimal\":1.2345678900123458E9}';
+testData['basicSimpleLiteralValues.smile'] = 'OikKAfqKZW1wdHlTdHJpbmcgg251bGwhhGZhbHNlIoN0cnVlI4RpbnQzMiQff39\/voZpbnQ2NF8xJSAAAACAhmludDY0XzIlP39\/f39\/f76GaW50NjRfMyVAAAAAAAAAgIZpbnQ2NF80JQN\/f39\/f39\/f76FYmlnaW50JokAIAAAAAAAAAAAAIZmbG9hdDMyKQBASVJISV0XRlSGZmxvYXQ2NCkAQTNjBRwfTTcniWJpZ2RlY2ltYWwpAEFpGTALJAMURvs=';
