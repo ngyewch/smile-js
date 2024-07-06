4b3a60cdfd0e641e821dc22f110a604600c220bd
Nick Ng <ngyewch@gmail.com>
2024-07-06 13:13:43 +0800

    refactor: removed comments

--- src/test/js/spec/SharedStringBufferSpec.ts
+++ src/test/js/spec/SharedStringBufferSpec.ts
@@ -51,31 +51,3 @@ t.test('when enabled', t=> {
 
     t.end();
 });
-
-/*
-    describe('when enabled', function () {
-        it('should succeed when adding a string', function () {
-            expect(ssb.addString('test1')).toBe('test1');
-            expect(ssb.getString(0)).toBe('test1');
-            expect(ssb.addString('test2')).toBe('test2');
-            expect(ssb.getString(1)).toBe('test2');
-            expect(ssb.addString('test3')).toBe('test3');
-            expect(ssb.getString(2)).toBe('test3');
-            expect(ssb.addString('test4')).toBe('test4');
-            expect(ssb.getString(3)).toBe('test4');
-        });
-
-        it('should reset when it reaches capacity', function () {
-            expect(ssb.addString('test1')).toBe('test1');
-            expect(ssb.addString('test2')).toBe('test2');
-            expect(ssb.addString('test3')).toBe('test3');
-            expect(ssb.addString('test4')).toBe('test4');
-            expect(ssb.addString('test5')).toBe('test5');
-            expect(ssb.getString(0)).toBe('test5');
-            expect(ssb.addString('test6')).toBe('test6');
-            expect(ssb.getString(1)).toBe('test6');
-            expect(function () {
-                ssb.getString(2);
-            }).toThrowError(Smile.SmileError);
-        });
-*/
