7199e9d72b627f6cbd638b484a7449ff3dd0f113
Nick Ng <ngyewch@gmail.com>
2024-07-08 11:28:54 +0800

    refactor: reorganized test data

--- src/test/js/spec/ParserSpec.ts
+++ src/test/js/spec/ParserSpec.ts
@@ -2,7 +2,7 @@ import t from 'tap';
 import {verifyFiles} from './utils.js';
 import path from 'path';
 
-const testSuiteDir = "src/test/data";
+const testSuiteDir = "src/test/data/basic";
 
 t.test('basic test suite', t => {
     verifyFiles(t, path.resolve(testSuiteDir, "*.smile"));
