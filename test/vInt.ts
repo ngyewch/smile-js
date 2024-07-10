ce4164d796713d9f066c8f9bcb3491f542340d3c
Nick Ng <ngyewch@gmail.com>
2024-07-10 13:50:50 +0800

    feat: updated tests

--- test/vInt.ts
+++ test/vInt.ts
@@ -1,9 +1,32 @@
-import t from 'tap';
+import t, {Test} from 'tap';
 import {VInt} from '../src/vInt.js';
+import {SmileError} from '../src/error.js';
+import {InputStream} from '../src/inputStream.js';
+import {throws} from './utils/assert.js';
 
 t.test('should decode VInt values', t => {
-    t.equal(VInt.decode(new Uint8Array([0x15, 0x2a, 0x55, 0x2a, 0xaa])), 2863311530);
-    t.equal(VInt.decode(new Uint8Array([0x01, 0x2a, 0x55, 0x2a, 0x55, 0x2a, 0x55, 0x2a, 0xa9])), BigInt('48038396025285289'));
+    decodeAndRead(t, [0x15, 0x2a, 0x55, 0x2a, 0xaa], 2863311530);
+    decodeAndRead(t, [0x01, 0x2a, 0x55, 0x2a, 0x55, 0x2a, 0x55, 0x2a, 0xa9], BigInt('48038396025285289'));
+
+    throws(t, () => {
+        VInt.decode(new Uint8Array([]));
+    }, e => e instanceof SmileError)
+
+    throws(t, () => {
+        VInt.decode(new Uint8Array([0xaa, 0xab]));
+    }, e => e instanceof SmileError)
+
+    throws(t, () => {
+        VInt.decode(new Uint8Array([0x31, 0x32]));
+    }, e => e instanceof SmileError)
 
     t.end();
 });
+
+function decodeAndRead(t: Test, elements: number[], wanted: number | bigint): void {
+    const encodedBytes = new Uint8Array(elements);
+    t.equal(VInt.decode(encodedBytes), wanted);
+
+    const inputStream = new InputStream(encodedBytes);
+    t.equal(VInt.read(inputStream), wanted);
+}
