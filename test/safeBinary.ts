00b6a1bce87ab0e9f0fb1638f35a75194e76ca26
Nick Ng <ngyewch@gmail.com>
2024-07-10 14:26:46 +0800

    feat: updated tests

--- test/safeBinary.ts
+++ test/safeBinary.ts
@@ -1,11 +1,36 @@
 import t from 'tap';
-import {arrayEqual} from './utils/assert.js';
+import {arrayEqual, throws} from './utils/assert.js';
 import {SafeBinary} from '../src/safeBinary.js';
+import {InputStream} from '../src/inputStream.js';
+import {SmileError} from '../src/error.js';
 
 t.test('should decode safe binary encoded bits', t => {
-    const wanted = new Uint8Array([0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
-    const data = new Uint8Array([0x00, 0x3f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x03]);
-    arrayEqual(t, SafeBinary.decode(data, wanted.length), wanted);
+    {
+        const decodedData = new Uint8Array([0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+        const encodedData = new Uint8Array([0x00, 0x3f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x03]);
+        arrayEqual(t, SafeBinary.decode(encodedData, decodedData.length), decodedData);
+    }
+
+    {
+        const encodedData = new Uint8Array([0x80]);
+        t.equal(SafeBinary.readBigInt(new InputStream(encodedData)), BigInt(0));
+    }
+
+    {
+        const encodedData = new Uint8Array([0x90, 0x7f, 0x040, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+        t.equal(SafeBinary.readBigInt(new InputStream(encodedData)), BigInt("-1329227995784915872903807060280344576"));
+    }
+    {
+        const encodedData = new Uint8Array([0x90, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+        t.equal(SafeBinary.readBigInt(new InputStream(encodedData)), BigInt("1329227995784915872903807060280344576"));
+    }
+
+    {
+        const encodedData = new Uint8Array([0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0xff]);
+        throws(t, () => {
+            SafeBinary.read(new InputStream(encodedData));
+        }, e => e instanceof SmileError);
+    }
 
     t.end();
 });
