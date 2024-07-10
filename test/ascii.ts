a649abfdde242e41f935103a6e6c7bd226d76543
Nick Ng <ngyewch@gmail.com>
2024-07-10 13:39:06 +0800

    feat: updated tests

--- test/ascii.ts
+++ test/ascii.ts
@@ -1,8 +1,15 @@
 import t from 'tap';
 import {ASCII} from '../src/ascii.js';
+import {InputStream} from '../src/inputStream.js';
 
 t.test('should decode ASCII values', t => {
-    t.equal(ASCII.decode(new Uint8Array([0x61, 0x62, 0x63])), 'abc');
+    const encodedBytes = new Uint8Array([0x61, 0x62, 0x63]);
+    const wanted = 'abc';
+
+    t.equal(ASCII.decode(encodedBytes), wanted);
+
+    const inputStream = new InputStream(encodedBytes);
+    t.equal(ASCII.read(inputStream, encodedBytes.length), wanted);
 
     t.end();
 });
--- test/utf8.ts
+++ test/utf8.ts
@@ -1,8 +1,15 @@
 import t from 'tap';
 import {UTF8} from '../src/utf8.js';
+import {InputStream} from '../src/inputStream.js';
 
 t.test('should decode UTF-8 values', t => {
-    t.equal(UTF8.decode(new Uint8Array([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0xe6, 0x82, 0xa8, 0xe5, 0xa5, 0xbd])), 'Hello 您好');
+    const encodedBytes = new Uint8Array([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0xe6, 0x82, 0xa8, 0xe5, 0xa5, 0xbd]);
+    const wanted = 'Hello 您好';
+
+    t.equal(UTF8.decode(encodedBytes), wanted);
+
+    const inputStream = new InputStream(encodedBytes);
+    t.equal(UTF8.read(inputStream, encodedBytes.length), wanted);
 
     t.end();
 });
