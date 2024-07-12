996b760f944e88c67a4757df9b9a1c74f63e77d8
Nick Ng <ngyewch@gmail.com>
2024-07-12 19:16:45 +0800

    refactor: fixed length big-endian

--- src/fixedLengthBigEndian.ts
+++ src/fixedLengthBigEndian.ts
@@ -5,7 +5,8 @@ import {calcByteLen} from './utils.js';
 import {Float32, Float64} from './float.js';
 
 export class FixedLengthBigEndian {
-    public static decode(bytes: Uint8Array, decodedByteLen: number): Uint8Array {
+    public static decode(bytes: Uint8Array): Uint8Array {
+        const decodedByteLen = calcByteLen(bytes.length, 7, 8);
         const arrayBuffer = new ArrayBuffer(decodedByteLen);
         const bitView = new BitView(arrayBuffer);
         bitView.bigEndian = true;
@@ -24,7 +25,7 @@ export class FixedLengthBigEndian {
     public static read(inputStream: InputStream, decodedByteLen: number): Uint8Array {
         const encodedByteLen = calcByteLen(decodedByteLen, 8, 7);
         const encodedBytes = inputStream.readArray(encodedByteLen);
-        return FixedLengthBigEndian.decode(encodedBytes, decodedByteLen);
+        return FixedLengthBigEndian.decode(encodedBytes);
     }
 
     public static readFloat32(inputStream: InputStream): number {
--- test/fixedLengthBigEndian.ts
+++ test/fixedLengthBigEndian.ts
@@ -11,7 +11,7 @@ t.test('should decode/encode fixed-length big-endian encoded bits', t => {
         const byteLength = 4;
         const floatEncodedData = new Uint8Array([0x44, 0x9a, 0x52, 0x25]);
         const fixedLengthBigEndianEncodedData = new Uint8Array([0x04, 0x24, 0x69, 0x24, 0x25]);
-        arrayEqual(t, FixedLengthBigEndian.decode(fixedLengthBigEndianEncodedData, byteLength), floatEncodedData);
+        arrayEqual(t, FixedLengthBigEndian.decode(fixedLengthBigEndianEncodedData), floatEncodedData);
         arrayEqual(t, FixedLengthBigEndian.read(new InputStream(fixedLengthBigEndianEncodedData), byteLength), floatEncodedData);
         approx(t, FixedLengthBigEndian.readFloat32(new InputStream(fixedLengthBigEndianEncodedData)), value, epsilon);
 
@@ -26,7 +26,7 @@ t.test('should decode/encode fixed-length big-endian encoded bits', t => {
         const byteLength = 8;
         const floatEncodedData = new Uint8Array([0x41, 0x67, 0x8c, 0x29, 0xc3, 0xf3, 0x5b, 0xa7]);
         const fixedLengthBigEndianEncodedData = new Uint8Array([0x00, 0x41, 0x33, 0x63, 0x05, 0x1c, 0x1f, 0x4d, 0x37, 0x27]);
-        arrayEqual(t, FixedLengthBigEndian.decode(fixedLengthBigEndianEncodedData, byteLength), floatEncodedData);
+        arrayEqual(t, FixedLengthBigEndian.decode(fixedLengthBigEndianEncodedData), floatEncodedData);
         arrayEqual(t, FixedLengthBigEndian.read(new InputStream(fixedLengthBigEndianEncodedData), byteLength), floatEncodedData);
         approx(t, FixedLengthBigEndian.readFloat64(new InputStream(fixedLengthBigEndianEncodedData)), value, epsilon);
 
