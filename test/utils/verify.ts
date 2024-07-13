6b81f8b1c3654b06e0a768f6e276e2b43c3ae5e0
Nick Ng <ngyewch@gmail.com>
2024-07-13 11:37:49 +0800

    refactor: tests

--- test/utils/verify.ts
+++ test/utils/verify.ts
@@ -3,8 +3,9 @@ import {globSync} from 'glob';
 import path from 'path';
 import fs from 'fs';
 import {execSync, type SpawnSyncReturns} from 'child_process';
-import {parse as jsonParse, stringify as jsonStringify, isInteger} from 'lossless-json';
+import {parse, stringify, isInteger} from 'lossless-json';
 import {decode} from '../../src/decoder.js';
+import {encode} from '../../src/encoder.js';
 import {objectEqual} from './assert.js';
 
 export function verifyFiles(t: Test, pattern: string | string[]): void {
@@ -12,8 +13,7 @@ export function verifyFiles(t: Test, pattern: string | string[]): void {
         nodir: true,
     })
         .filter(smileFile => {
-            const parsedPath = path.parse(smileFile);
-            const jsonFile = path.resolve(parsedPath.dir, parsedPath.name + ".json");
+            const jsonFile = replaceExtension(smileFile, '.json');
             return fs.existsSync(jsonFile);
         });
     for (const smileFile of smileFiles) {
@@ -32,49 +32,30 @@ export function verifyFile(t: Test, smileFile: string): void {
     const relativePath = path.relative(process.cwd(), smileFile);
     //console.log(`[${relativePath}] ------------------------------`);
     t.test(relativePath, t => {
-        const parsedPath = path.parse(smileFile);
-        const jsonFile = path.resolve(parsedPath.dir, parsedPath.name + ".json");
-        const smileData = fs.readFileSync(smileFile);
-        const jsonData = fs.readFileSync(jsonFile);
-        const smileValue = decode(smileData);
-        const wrappedJsonValue = jsonParse(jsonData.toString(), null, v => {
-            if (isInteger(v)) {
-                const b = BigInt(v);
-                if ((b >= BigInt(Number.MIN_SAFE_INTEGER)) && (b <= BigInt(Number.MAX_SAFE_INTEGER))) {
-                    return Number(b);
-                } else {
-                    return b;
-                }
-            } else {
-                return parseFloat(v);
-            }
-        }) as WrappedJSONValue;
+        const jsonFile = replaceExtension(smileFile, '.json');
+        const smileValue = loadSmileFromFile(smileFile);
+        const wrappedJsonValue = loadWrappedJsonFromFile(jsonFile);
         const jsonValue = wrappedJsonValue.value;
         const pass = objectEqual(t, smileValue, jsonValue);
-        const skipJsonDiff = relativePath.startsWith('testdata/serde-smile/double/')
-            || relativePath.startsWith('testdata/serde-smile/float/');
 
-        if (pass && !skipJsonDiff) {
+        const outputWrappedJsonValue = {
+            ...wrappedJsonValue,
+            value: smileValue,
+        };
+        const outputJsonFile = path.resolve('build/test-output',
+            path.relative(process.cwd(), replaceExtension(smileFile, ".json")));
+        saveWrappedJsonToFile(outputJsonFile, outputWrappedJsonValue);
+
+        if (pass && !skipJsonDiff(relativePath)) {
             t.test('json diff', t => {
-                wrappedJsonValue.value = smileValue;
-                const outputJson = jsonStringify(wrappedJsonValue);
-                if (outputJson !== undefined) {
-                    const parsedPath2 = path.parse(relativePath);
-                    const outputJsonFile = path.resolve('build/test-output', parsedPath2.dir, parsedPath2.name + ".json");
-                    fs.mkdirSync(path.parse(outputJsonFile).dir, {recursive: true});
-                    fs.writeFileSync(outputJsonFile, outputJson);
-
-                    const jsonDiffCmd = `./json-diff ${jsonFile} ${outputJsonFile}`;
-                    try {
-                        const jsonDiff = execSync(jsonDiffCmd);
-                    } catch (e) {
-                        const returns = e as SpawnSyncReturns<Buffer>;
-                        t.fail({
-                            doNotWant: returns.stdout.toString(),
-                        });
-                    }
-                } else {
-                    t.fail('could not write output JSON');
+                const jsonDiffCmd = `./json-diff ${jsonFile} ${outputJsonFile}`;
+                try {
+                    const jsonDiff = execSync(jsonDiffCmd);
+                } catch (e) {
+                    const returns = e as SpawnSyncReturns<Buffer>;
+                    t.fail({
+                        doNotWant: returns.stdout.toString(),
+                    });
                 }
 
                 t.end();
@@ -83,4 +64,44 @@ export function verifyFile(t: Test, smileFile: string): void {
 
         t.end();
     });
+}
+
+function noSkipJsonDiff(relativePath: string): boolean {
+    return false;
+}
+
+function skipJsonDiff(relativePath: string): boolean {
+    return relativePath.startsWith('testdata/serde-smile/double/')
+        || relativePath.startsWith('testdata/serde-smile/float/');
+}
+
+function replaceExtension(p: string, ext: string): string {
+    const parsedPath = path.parse(p);
+    return path.resolve(parsedPath.dir, parsedPath.name + ext);
+}
+
+function parseNumber(v: string): unknown {
+    if (isInteger(v)) {
+        const b = BigInt(v);
+        if ((b >= BigInt(Number.MIN_SAFE_INTEGER)) && (b <= BigInt(Number.MAX_SAFE_INTEGER))) {
+            return Number(b);
+        } else {
+            return b;
+        }
+    } else {
+        return parseFloat(v);
+    }
+}
+
+function loadWrappedJsonFromFile(filename: string): WrappedJSONValue {
+    return parse(fs.readFileSync(filename).toString(), null, parseNumber) as WrappedJSONValue;
+}
+
+function loadSmileFromFile(filename: string): any {
+    return decode(fs.readFileSync(filename));
+}
+
+function saveWrappedJsonToFile(filename: string, wrappedJsonValue: WrappedJSONValue): void {
+    fs.mkdirSync(path.parse(filename).dir, {recursive: true});
+    fs.writeFileSync(filename, stringify(wrappedJsonValue));
 }
