cd64978d5d96f50d0ebbdb967f9445242f256b72
Nick Ng <ngyewch@gmail.com>
2024-07-13 11:37:22 +0800

    fix: encode function name

--- src/encoder.ts
+++ src/encoder.ts
@@ -25,7 +25,7 @@ const version = 0;
  * @param value value
  * @param options encoder options
  */
-export function decode(value: any, options?: Partial<EncoderOptions>): any {
+export function encode(value: any, options?: Partial<EncoderOptions>): any {
     return new EncoderContext(value, options).encode();
 }
 
